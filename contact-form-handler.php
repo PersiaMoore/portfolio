af275a783853c5dbb6e5e5eb7bf76749ed70f111
PersiaMoore <persiasimonemoore@gmail.com>
2020-06-25 20:41:29 +0200

    portfolio v3

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,23 +1,23 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
 
 
-$email_from = 'persiamooredesigns@persiamoore.com';
+    $email_from = 'persiamooredesigns@persiamoore.com';
 
-$email_subject = "New Form Submission";
+    $email_subject = "New Form Submission";
 
-$email_body = "User Name: $name.\n".
-                "User Email: $visitor_email.\n".
-                    "User Message: $message.\n";
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Message: $message.\n";
 
 
                     $to = "persiamooredesigns@gmail.com";
 
                     $headers = "From: $email_from \r\n";
 
-                    $headres .= "Reply-To: $visitor_email \r\n";
+                    $headers .= "Reply-To: $visitor_email \r\n";
 
                     mail($to,$email_subject,$email_body,$headers);
 
